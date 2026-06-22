3725097b3ad1d063b32f664755102418a7f1cbcb
Boikanyo-Mohlamonyane <Boikanyokanyo481@gmail.com>
2026-06-22 13:41:12 +0200

    AuthReqest message

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,5 +1,35 @@
 package com.example.demo.dto;
 
 public class AuthRequest {
-        p
+
+        private String username;
+        private String password;
+        private String phoneNumber;
+
+        public class AuthRequest() {
+        } 
+        
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+        public String getPassword() {
+            return password;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
+        public String getPhoneNumber() {
+            return phoneNumber;
+
+        }
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+        
+
+
 }
